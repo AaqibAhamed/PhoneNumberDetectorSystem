5008b3638ff791407a2d8ca96623ce6f705d085e
Ahamed Aaqib <aaqibgahamed@gmail.com>
2024-05-27 13:33:33 +0530

    Add PhoneNumberOutput

---
+++ PhoneNumberDetectorSystem/Services/PhoneNumberOutput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNumberDetectorSystem.Services
+{
+    public class PhoneNumberOutput 
+    {
+        public void DisplayOutput(string output)
+        {
+            Console.WriteLine(output);
+        }
+    }
+}
